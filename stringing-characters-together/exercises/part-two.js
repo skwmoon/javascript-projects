2766c486ee5a944348951b534232e52e28434342
Sarah White <skwmoon@gmail.com>
2024-05-09 14:40:58 -0500

    Stringing-characters-together

--- stringing-characters-together/exercises/part-two.js
+++ stringing-characters-together/exercises/part-two.js
@@ -1,32 +1,52 @@
 //Part Two Section One
 
 let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
+let newString = dna.trim().toUpperCase();
+let newDna = dna.replace(dna,newString);
 
 // First, print out the dna strand in it's current state.
 
+
 //1) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
 
-console.log(/* Your code here. */);
+console.log(newString);
 
 //2) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
-console.log();
+console.log(dna);
 
 //3) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
 
-console.log(dna);
+console.log(newDna);
 
 //Part Two Section Two
 
 let dnaTwo = "TCG-TAC-GAC-TAC-CGT-CAG-ACT-TAA-CCA-GTC-CAT-AGA-GCT";
+let characterlength = 40;
 
 //1) Replace the gene "GCT" with "AGG", and then print the altered strand.
 
+dnaTwo = dnaTwo.replace('GCT','AGG');
+console.log(dnaTwo);
+
 //2) Look for the gene "CAT" with ``indexOf()``. If found print, "CAT gene found", otherwise print, "CAT gene NOT found".
+
+console.log(dnaTwo.indexOf("CAT"));
+if (dnaTwo.indexOf("CAT")){
+    console.log("CAT gene found");
+} else {
+    console.log("CAT gene NOT found");
+}
 
 //3) Use .slice() to print out the fifth gene (set of 3 characters) from the DNA strand.
 
+console.log(dnaTwo.slice(16,19));
+
 //4) Use a template literal to print, "The DNA strand is ___ characters long."
 
+console.log(`The DNA strand is ${characterlength} characters long.`);
+
 //5) Just for fun, apply methods to ``dna`` and use another template literal to print, 'taco cat'.
+
+console.log(`${dnaTwo.slice(4,7).toLowerCase()}o ${dnaTwo.slice(dnaTwo.indexOf('CAT'),dnaTwo.indexOf('CAT')+3).toLowerCase()}`);
