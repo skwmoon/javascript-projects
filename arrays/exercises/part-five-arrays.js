c17b04b5fb89a1d320aad02b9ac4535068720bdd
Sarah White <skwmoon@gmail.com>
2024-05-14 10:35:27 -0500

    Chapter 8 Arrays

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -1,11 +1,34 @@
 let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
+let newstr = '';
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
 
+console.log(str.split());
+console.log(str.split('e'));
+console.log(str.split(' '));
+console.log(str.split(''));
+
+console.log("split divides a string into smaller pieces which are then stored in a new array.");
+
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
+
+newstr = arr.join('+');
+console.log(newstr);
+
+newstr = arr.join('');
+console.log(newstr);
+
+newstr = arr.join('_');
+console.log(newstr);
+
+console.log("join combines all elements of a n array into a string.");
 
 //3) Do split or join change the original string/array?
 
+console.log("Yes.");
+
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+console.log(cargoHold.split(',').sort().join(','));
--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -1,10 +1,37 @@
 let holdCabinet1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
 let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
+let holdCabinet3 = [];
 
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
 
+holdCabinet3 = holdCabinet1.concat(holdCabinet2);
+console.log(holdCabinet3);
+
+holdCabinet3 = holdCabinet2.concat(holdCabinet1);
+console.log(holdCabinet3);
+
+console.log(holdCabinet1.concat(holdCabinet2,holdCabinet1));
+
+console.log("holdCabinet1");
+
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
 
+let items = holdCabinet1.slice(1,3);
+console.log(items);
+
+let items2 = holdCabinet2.slice(0,2);
+console.log(items2);
+
+console.log('slice does not alter the original arrays.');
+
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+
+holdCabinet2.sort();
+console.log(holdCabinet2);
+
+console.log("The difference between the two methods are that reverse is starting from the end of the array and sort is sorting the array in increasing order. The two methods do not alter the original array.");
